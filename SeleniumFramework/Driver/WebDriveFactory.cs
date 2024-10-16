93f8272338aa9a0d2b26c8dabbf5b3cfc233cc42
LUIS RIQUELME ROMERO <luis.riquelme@novofarma.com>
2024-10-16 08:19:10 -0300

    prueba

--- SeleniumFramework/Driver/WebDriveFactory.cs
+++ SeleniumFramework/Driver/WebDriveFactory.cs
@@ -5,12 +5,17 @@ namespace SeleniumFramework.Drivers
 {
     public class WebDriveFactory
     {
+        /// <summary>
+        /// maximiza pantalla
+        /// </summary>
+        /// <returns></returns>
         public static IWebDriver CreateWebDriver()
         {
 
             var options = new ChromeOptions();
             options.AddArguments("--Start-maximized");
             return new ChromeDriver(options);
+
 
 
 
